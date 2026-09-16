55951dbf8f2845297937aaa0996c81a5be5337b6
florian-billon <154011027+florian-billon@users.noreply.github.com>
2026-09-16 19:26:47 +0200

    Increase z-index of content to z-50 to prevent click blocking

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <ServiceWorkerRegister />
           <div className="relative w-full min-h-screen">
             <div className="fixed inset-0 bg-[url('/background-clouds.png')] bg-cover bg-center bg-no-repeat brightness-[0.7] contrast-[1.1] z-0 pointer-events-none" />
-            <div className="relative z-10">
+            <div className="relative z-50">
               {children}
             </div>
           </div>
